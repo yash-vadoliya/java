8d6ecc90d69023659d4cce28a7d365f7a35c2490
Yash  Vadoliya <104588907+yash-vadoliya@users.noreply.github.com>
2024-03-12 13:05:29 +0530

    Update Result.java

--- Package/Result.java
+++ Package/Result.java
@@ -1,13 +1,10 @@
 import pack.*;
-import add.*;
-
 
 class Result
 {
 	public static void main(String[] args) 
 	{
-		// Dis d = new Dis();
-		// d.display();
-		Add a = new Add();
+		 Dis d = new Dis();
+		 d.display();
 	}
 }
